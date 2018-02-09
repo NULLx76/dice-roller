9b99c38a9f28a0c221168feb9235bd6b84d1e867
victor <victor@xirion.net>
2018-02-09 16:40:26 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Dice-Roller',
-    version='0.3',
+    version='0.4',
     long_description=open('README.md').read(),
     scripts = [
             'roll',
