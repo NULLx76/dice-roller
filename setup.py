23c02f70ceea64ce9ece23931c17e795bbf0db70
victor <victor@xirion.net>
2018-02-09 16:47:52 +0100

    Update 'setup.py'

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='Dice-Roller',
     version='0.4',
+  	license='GPL3',
     long_description=open('README.md').read(),
     scripts = [
             'roll',
         ]
 )
-
 
