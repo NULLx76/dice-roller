15eb2daa2555d13095288d6c56b53c29bebacca7
Victor <victor@xirion.net>
2018-02-15 19:59:09 +0100

    renamed old file to old_roll and roll2 to roll, also updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from distutils.core import setup
 
 setup(
     name='Dice-Roller',
-    version='0.4',
-  	license='GPL3',
+    version='1',
+    license='GPL3',
     long_description=open('README.md').read(),
-    scripts = [
-            'roll',
-        ]
+    scripts=[
+        'roll',
+    ]
 )
-
