34b91e1c633e91f7431af99cdf6f2f0fe637a102
Victor Roest <victor@xirion.net>
2018-02-17 19:39:41 +0100

    Testing pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@ from distutils.core import setup
 
 setup(
     name='Dice-Roller',
-    version='1.1',
+    version='1.2',
     license='GPL3',
+    author = 'Victor Roest',
+    author_email = 'victor@xirion.net',
+    url = 'https://git.xirion.net/victor/dice-roller',
+    download_url = 'https://git.xirion.net/victor/dice-roller/archive/v1.2.tar.gz',
+    keywords = ['dnd', 'dice'],
+    description= 'A dice-rolling application using standard dice notation',
     long_description=open('README.md').read(),
     scripts=[
         'roll',
