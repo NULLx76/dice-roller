49c88b56ba96ca20d84420e1850563fde5f7d56b
Victor Roest <victor@xirion.net>
2018-02-17 20:54:05 +0100

    Hopefully fixed pandoc

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from distutils.core import setup
 
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = pypandoc.convert('README.md', 'rst', format='md')
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
 setup(
     name='Dice-Roller',
-    version='1.3',
+    version='1.3.1',
     license='GPL3',
     author = 'Victor Roest',
     author_email = 'victor@xirion.net',
     url = 'https://git.xirion.net/victor/dice-roller',
-    download_url = 'https://git.xirion.net/victor/dice-roller/archive/v1.3.tar.gz',
+    download_url = 'https://git.xirion.net/victor/dice-roller/archive/v1.3.1.tar.gz',
     keywords = ['dnd', 'dice'],
     description= 'A dice-rolling application using standard dice notation',
     long_description=long_description,
