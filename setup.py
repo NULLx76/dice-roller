67f86fd7a614b97760897401fd7625e6c112bf27
Victor Roest <victor@xirion.net>
2018-04-13 21:29:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ except Exception:
 
 setup(
     name='Dice-Roller',
-    version='1.6.2',
+    version='1.6.2.5',
     license='GPL3',
     author = 'Victor Roest',
     author_email = 'victor@xirion.net',
     url = 'https://gitlab.xirion.net/vroest/dice-roller',
-    download_url = 'https://gitlab.xirion.net/vroest/dice-roller/repository/v1.6.2/archive.tar.gz',
+    download_url = 'https://gitlab.xirion.net/vroest/dice-roller/repository/v1.6.2.5/archive.tar.gz',
     keywords = ['dnd', 'dice'],
     description= 'A dice-rolling application using standard dice notation',
     long_description=long_description,
