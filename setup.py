6be91db44850cd4fa8f73419f9fa89d29db7d480
Victor Roest <victor@xirion.net>
2018-04-13 22:19:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 #from distutils.core import setup
 from setuptools import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst', format='md')
-except Exception:
-    long_description = open('README.md').read()
+
+long_description = open('README.rst').read()
 
 setup(
     name='Dice-Roller',
-    version='1.6.2.7',
+    version='1.6.2.8',
     license='GPL3',
     author = 'Victor Roest',
     author_email = 'victor@xirion.net',
     url = 'https://gitlab.xirion.net/vroest/dice-roller',
-    download_url = 'https://gitlab.xirion.net/vroest/dice-roller/repository/v1.6.2.7/archive.tar.gz',
+    download_url = 'https://gitlab.xirion.net/vroest/dice-roller/repository/v1.6.2.8/archive.tar.gz',
     keywords = ['dnd', 'dice'],
     description= 'A dice-rolling application using standard dice notation',
     long_description=long_description,
