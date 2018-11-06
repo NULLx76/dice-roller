8f0ed5acf2fa77431977aed3f6933d9a91160b32
Victor <victor@xirion.net>
2018-11-06 10:24:11 +0100

    Bumped version to 1.9

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ long_description = open('README.rst').read()
 
 setup(
     name='Dice-Roller',
-    version='1.8',
+    version='1.9',
     license='GPL3',
     author = 'Victor Roest',
     author_email = 'victor@xirion.net',
     url = 'https://gitlab.xirion.net/vroest/dice-roller',
-    download_url = 'https://gitlab.xirion.net/vroest/dice-roller/repository/v1.8/archive.tar.gz',
+    download_url = 'https://gitlab.xirion.net/vroest/dice-roller/repository/v1.9/archive.tar.gz',
     keywords = ['dnd', 'dice'],
     description= 'A dice-rolling application using standard dice notation',
     long_description=long_description,
